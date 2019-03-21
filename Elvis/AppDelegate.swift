b52ad8f6cb52def12ecb4ed92156e051fbc534fb
Benas <benasmiliunas@gmail.com>
2019-03-21 16:08:23 +0200

    -

--- Elvis/AppDelegate.swift
+++ Elvis/AppDelegate.swift
@@ -16,8 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        //Theme.darkTheme()
-        //Theme.defaultTheme()
+        UIApplication.shared.statusBarStyle = .lightContent
         return true
     }
 
